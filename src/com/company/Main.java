51e2dd5d801edd7d8b46c4c1320b60706c3580f9
Max44444 <Skorokhodov.m.v@ukr.net>
2021-03-23 21:31:16 +0200

    singleton pattern

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,29 +1,15 @@
 package com.company;
 
-import com.company.factory.EnemyShip;
-import com.company.factory.EnemyShipFactory;
 
-import java.util.Scanner;
+import com.company.singleton.GetTheTiles;
 
 public class Main {
 
     public static void main(String[] args) {
-        EnemyShipFactory shipFactory = new EnemyShipFactory();
-        EnemyShip theEnemy = null;
-        Scanner scanner = new Scanner(System.in);
+        Runnable getTiles = new GetTheTiles();
+        Runnable getTilesAgain = new GetTheTiles();
 
-        if (scanner.hasNextLine()) {
-            theEnemy = shipFactory.makeEnemyShip(scanner.nextLine());
-        }
-
-        if (theEnemy != null) {
-            doStuffEnemy(theEnemy);
-        }
-    }
-
-    private static void doStuffEnemy(EnemyShip anEnemyShip) {
-        anEnemyShip.displayEnemyShip();
-        anEnemyShip.followHeroShip();
-        anEnemyShip.enemyShipShoots();
+        new Thread(getTiles).start();
+        new Thread(getTilesAgain).start();
     }
 }

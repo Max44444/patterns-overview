91b0a1deb6821c0f6e88070bdb39f93e1d2391d0
Max44444 <Skorokhodov.m.v@ukr.net>
2021-03-28 10:40:24 +0300

    bridge design pattern

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,29 @@
 package com.company;
 
-import com.company.facade.BankAccountFacade;
+import com.company.bridge.RemoteButton;
+import com.company.bridge.TVDevice;
+import com.company.bridge.TVRemoteMute;
+import com.company.bridge.TVRemotePause;
 
 public class Main {
 
     public static void main(String[] args) {
-        BankAccountFacade accessingBank = new BankAccountFacade(12345678, 1234);
-        accessingBank.withdrawCash(50.00);
-        accessingBank.withdrawCash(900.00);
-        accessingBank.depositCash(200.00);
+        RemoteButton theTV = new TVRemoteMute(new TVDevice(1, 200));
+        RemoteButton theTV2 = new TVRemotePause(new TVDevice(1, 200));
+
+        System.out.println("Test TV with Mute");
+        theTV.buttonFivePressed();
+        theTV.buttonSixPressed();
+        theTV.buttonNinePressed();
+
+        System.out.println("\nTest TV with Pause");
+        theTV2.buttonFivePressed();
+        theTV2.buttonSixPressed();
+        theTV2.buttonSixPressed();
+        theTV2.buttonSixPressed();
+        theTV2.buttonSixPressed();
+        theTV2.buttonNinePressed();
+        theTV2.deviceFeedback();
     }
 
 }
---
+++ src/com/company/bridge/TVRemotePause.java
@@ -0,0 +1,14 @@
+package com.company.bridge;
+
+public class TVRemotePause extends RemoteButton {
+
+    public TVRemotePause(EntertainmentDevice newDevice) {
+        super(newDevice);
+    }
+
+    @Override
+    public void buttonNinePressed() {
+        System.out.println("TV was Paused");
+    }
+
+}
